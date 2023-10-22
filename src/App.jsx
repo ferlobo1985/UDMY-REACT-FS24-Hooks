071c3fc76ddbaf55cc1ff7bdc2f0416685b7db96
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-22 02:45:34 -0300

    04-Creating a scenario
    04-Creating a scenario

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,32 @@
-import { useState, useEffect } from 'react'
-import BlockComponent from './block';
+import { useState } from "react";
+import Title from "./components/title";
+import Count from "./components/count";
+import CountBtn from './components/countBtn';
+import Age from "./components/age";
+import AgeBtn from "./components/ageBtn";
 
 const App = () => {
-  let [count,setCount] = useState(0);
-  let [animal,setAnimal] = useState('Dog');
-  let [block,setBlock] = useState(false)
+    let [count,setCount] = useState(0);
+    let [age,setAge] = useState(10);
 
-  const addOne  = () => setCount(count+1);
-  const restOne = () => setCount(count-1)
-  const setCero = () => setCount(0)
-
-
-  
-  useEffect(()=>{
-    return ()=>{
-      console.log('Use effect count')
+    const handleCount = () => {
+        setCount(count+1)
     }
-  },[count])
 
+    const handleAge = () => {
+        setAge(age+1)
+    }
 
-  // useEffect(()=>{
-  //   console.log(count)
-  // },[count,animal])
+    return(
+        <>
+            <Title/>
+            <Count count={count}/>
+            <CountBtn handleCount={handleCount}/>
+            <Age age={age}/>
+            <AgeBtn handleAge={handleAge}/>
+        </>
+    )
 
-
-  // useEffect(()=>{
-  //   console.log(animal)
-  // },[animal])
-
-
-  return(
-    <>
-      <h3>Count: {count}</h3>
-      <button onClick={addOne}>+1</button>
-      <button onClick={restOne}>-1</button>
-      <button onClick={setCero}>Set to 0</button>
-      <hr/>
-      <h3>{animal}</h3>
-      <button onClick={()=>setAnimal('Cat')}>Change animal</button>
-      <hr/>
-      { block ? 
-        <BlockComponent/>
-        :null
-      }
-      <button onClick={()=>setBlock(!block)}>Block</button>
-    </>
-  )
 }
 
 export default App;
---
+++ src/components/count.jsx
@@ -0,0 +1,12 @@
+const Count = ({count}) => {
+
+    console.log('2-Count')
+
+    return(
+        <>
+            <h3>The count is:{count}</h3>
+        </>
+    )
+}
+
+export default Count;
